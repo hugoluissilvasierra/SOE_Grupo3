34e7843fb57c27b72dafb7eee7316d92e108bb3e
juank007 <alavjc@gmail.com>
2021-11-05 21:33:09 -0400

    pagina de innicio

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+	<h1>Fundamentos de Ingeniería de Software</h1>
+	<h2>Grupo 3 - Integrantes</h2>
+<ul>
+	<li> <a href="noelVaca.html">Esnor Noel Enrique Vaca Moreno</a></li>
+	<li> <a href="HugoSilva.html">Hugo Luis Silva Sierra</a></li>
+	<li> <a href="gabrielCasas.html">Gabriel Casas Mamani</a></li>
+	<li> <a href="JavierSilva.html">Javier Silva Sierra</a></li>
+	<li> <a href="">Guiselle Oxa Flores</a></li>
+	<li> <a href="JuanChoqueAlavi.html">Juan Carlos Choque Alavi</a></li>
+	<li> <a href="jhonnyLimachi.html">Jhonny Marcelo Limachi</a></li>
+</ul>
+</body>
+</html>
